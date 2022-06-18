274d2948b634556f1201316833e85813143a00ca
dialaabulkhail <diala.sh.98@gmail.com>
2022-06-18 14:02:54 +0300

    edits

--- pages/components/Nav.js
+++ pages/components/Nav.js
@@ -49,41 +49,41 @@ function About() {
       className={
         shadow
           ? "fixed w-full h-20 shadow-xl shadow-teal-500 ease-in duration-300  z-[100]"
-          : "fixed w-full h-20 shadow-xl  z-[100]"
+          : "fixed w-full h-20 shadow-xl"
       }
     >
-      <div className="flex justify-between items-center w-full h-full px-10 2xl:px-16">
+      <div className="flex items-center justify-between w-full h-full px-10 2xl:px-16">
         <Image
           src="/../public/assets/logo.png"
           alt="logo"
-          width="135"
+          width="200"
           height="100"
         />
 
         <div>
           <ul style={{color: `${linkColor}`}} className="hidden md:flex">
             <Link href="/">
-              <li className="ml-10 text-sm uppercase hover:border-b border-teal-200">
+              <li className="ml-10 text-sm uppercase border-teal-200 hover:border-b">
                 Home
               </li>
             </Link>
             <Link href="/#about">
-              <li className="ml-10 text-sm uppercase hover:border-b border-teal-200">
+              <li className="ml-10 text-sm uppercase border-teal-200 hover:border-b">
                 About
               </li>
             </Link>
             <Link href="/#skills">
-              <li className="ml-10 text-sm uppercase hover:border-b border-teal-200">
+              <li className="ml-10 text-sm uppercase border-teal-200 hover:border-b">
                 Skills
               </li>
             </Link>
             <Link href="/#projects">
-              <li className="ml-10 text-sm uppercase hover:border-b border-teal-200">
+              <li className="ml-10 text-sm uppercase border-teal-200 hover:border-b">
                 Projects
               </li>
             </Link>
             <Link href="/#contact">
-              <li className="ml-10 text-sm uppercase hover:border-b border-teal-200">
+              <li className="ml-10 text-sm uppercase border-teal-200 hover:border-b">
                 Contact
               </li>
             </Link>
@@ -104,26 +104,26 @@ function About() {
               : "fixed left-[-100%] top-0 p-5 ease-in duration-500"
           }
         >
-          <div className="flex w-full items-center justify-between">
+          <div className="flex items-center justify-between w-full">
             <Image
               src="/../public/assets/logo.png"
-              width="135"
+              width="200"
               height="100"
               alt="logo"
             />
             <div
               onClick={handleNav}
-              className="rounded-full shadow-md shadow-teal-200 p-2 cursor-pointer hover:scale-110 ease-in duration-300"
+              className="p-2 duration-300 ease-in rounded-full shadow-md cursor-pointer shadow-teal-200 hover:scale-110"
             >
               <AiOutlineClose />
             </div>
           </div>
-          <div className="py-10 flex flex-col">
+          <div className="flex flex-col py-10">
             <ul className="uppercase">
               <Link href="/">
                 <li
                   onClick={() => setNav(false)}
-                  className="py-5 text-sm hover:border-b border-teal-200"
+                  className="py-5 text-sm border-teal-200 hover:border-b"
                 >
                   Home
                 </li>
@@ -131,7 +131,7 @@ function About() {
               <Link href="/#about">
                 <li
                   onClick={() => setNav(false)}
-                  className="py-5 text-sm hover:border-b border-teal-200"
+                  className="py-5 text-sm border-teal-200 hover:border-b"
                 >
                   About
                 </li>
@@ -139,7 +139,7 @@ function About() {
               <Link href="/#skills">
                 <li
                   onClick={() => setNav(false)}
-                  className="py-5 text-sm hover:border-b border-teal-200"
+                  className="py-5 text-sm border-teal-200 hover:border-b"
                 >
                   Skills
                 </li>
@@ -147,7 +147,7 @@ function About() {
               <Link href="/#projects">
                 <li
                   onClick={() => setNav(false)}
-                  className="py-5 text-sm hover:border-b border-teal-200"
+                  className="py-5 text-sm border-teal-200 hover:border-b"
                 >
                   Projects
                 </li>
@@ -155,34 +155,34 @@ function About() {
               <Link href="/#contact">
                 <li
                   onClick={() => setNav(false)}
-                  className="py-5 text-sm hover:border-b border-teal-200"
+                  className="py-5 text-sm border-teal-200 hover:border-b"
                 >
                   Contact
                 </li>
               </Link>
             </ul>
             <div className="py-10">
-              <p className="uppercase tracking-widest text-teal-600 py-3">
+              <p className="py-3 tracking-widest text-teal-600 uppercase">
                 Let's Connect!
               </p>
               <div className="flex items-center justify-between my-full w-full sm:w-[80%]">
-                <div className="rounded-full shadow-md shadow-teal-200 p-3 cursor-pointer hover:scale-110 ease-in duration-200">
+                <div className="p-3 duration-200 ease-in rounded-full shadow-md cursor-pointer shadow-teal-200 hover:scale-110">
                   <a href="https://www.linkedin.com/in/dialaabulkhail/">
                     <FaLinkedinIn />
                   </a>
                 </div>
-                <div className="rounded-full shadow-md shadow-teal-200 p-3 cursor-pointer hover:scale-110 ease-in duration-200">
+                <div className="p-3 duration-200 ease-in rounded-full shadow-md cursor-pointer shadow-teal-200 hover:scale-110">
                   <a href="https://github.com/dialaabulkhail">
                     <FaGithub />
                   </a>
                 </div>
-                <div className="rounded-full shadow-md shadow-teal-200 p-3 cursor-pointer hover:scale-110 ease-in duration-200">
+                <div className="p-3 duration-200 ease-in rounded-full shadow-md cursor-pointer shadow-teal-200 hover:scale-110">
                   <a href="mailto:diala.sh.98@gmail.com">
                     <AiOutlineMail />
                   </a>
                 </div>
-                <div className="rounded-full shadow-md shadow-teal-200 p-3 cursor-pointer hover:scale-110 ease-in duration-200">
-                  <a href="https://drive.google.com/file/d/1oiSbPPsi9AbiYuiMuhjNd907mLCjk4ND/view?usp=sharing">
+                <div className="p-3 duration-200 ease-in rounded-full shadow-md cursor-pointer shadow-teal-200 hover:scale-110">
+                  <a href="https://dialabk.hashnode.dev/">
                     <BsFillPersonLinesFill />
                   </a>
                 </div>
